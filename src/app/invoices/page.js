68ffab76d71b11bde5fe221628335cfea86c59ca
Avinash Kemparaju <kemparaju.avinash@gmail.com>
2026-02-17 18:24:54 +0530

    feat: update company address in OUR_COMPANY object

--- src/app/invoices/page.js
+++ src/app/invoices/page.js
@@ -32,7 +32,7 @@ const getMonthValue = () => {
 
 const OUR_COMPANY = {
   name: "SSM Cabs",
-  address: "No. 12, Anna Salai, Chennai, TN 600002",
+  address: "1st Floor, MSIL Building, Old Madras Rd, M V Extenstion, Hoskote, Bengaluru, Karnataka 562114",
   phone: "+91 44 4000 1234",
   email: "accounts@ssmcabs.com",
   bank_details: "Account: 000111222333 | SBI | IFSC: SBIN0001234",
